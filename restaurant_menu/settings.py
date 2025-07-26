6716cb1983c606cf96b95777bab31fe995b62de0
Shahab <113819469+shahabRDZ@users.noreply.github.com>
2025-07-27 02:49:52 +0330

    Add STATIC_ROOT for collectstatic

--- restaurant_menu/settings.py
+++ restaurant_menu/settings.py
@@ -63,6 +63,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'menu' / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
