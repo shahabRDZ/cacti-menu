1048d1ce8a00e561f43991610cb6c85006b142e6
Shahab <113819469+shahabRDZ@users.noreply.github.com>
2025-07-27 01:16:29 +0330

    Fix image fallback and logo path

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -7,3 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('menu.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
